34ca1214b9054412500c8dfbd86126ad6f8138db
PreCyz <precpaw@op.pl>
2021-10-06 22:36:43 +0200

    Programmatic jcache.

--- src/main/java/pg/hib/Main.java
+++ src/main/java/pg/hib/Main.java
@@ -8,6 +8,9 @@ import pg.hib.entities.CarEntity;
 import pg.hib.entities.TestEntity;
 import pg.hib.providers.HibernateSessionProvider;
 
+import javax.cache.*;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -33,24 +36,6 @@ public class Main {
         repository.deleteAll(repository.findAll());
 
         simpleOpr(repository);
-/*
-        List<TestEntity> all = repository.findAll();
-        LOGGER.info("This is what I got from db {}", all);
-
-        all = repository.findByIds(Stream.of(1, 3).collect(toSet()));
-        LOGGER.info("This is what I got from db {}", all);
-
-        Optional<TestEntity> testBean = repository.findById(4);
-        testBean.ifPresent(tb -> LOGGER.info("This is what I got from db {}", tb));
-
-        List<TestEntity> activeEntities = repository.findByActive(true);
-        LOGGER.info("Only active entities {}", activeEntities);
-
-        repository.save(new TestEntity(false, LocalDateTime.now()));
-        List<TestEntity> inactiveEntities = repository.findByActive(false);
-        LOGGER.info("Only inactive entities {}", inactiveEntities);
-
-        batchTestBeanSave(repository);*/
     }
 
     private static void simpleOpr(TestEntityDao repository) {
@@ -92,52 +77,6 @@ public class Main {
 
         LOGGER.info("Getting cars by id {}", generateIds.get(0));
         repository.findById(generateIds.get(0)).ifPresent(e -> LOGGER.info("car: {}", e));
-
-        /*
-        CacheManager cacheManager = CacheManager.ALL_CACHE_MANAGERS.get(0);
-        LOGGER.info("Cache Manager Name {}", cacheManager.getName());
-        Cache carsCache = cacheManager.getCache("cars");
-        LOGGER.info("Cars cache {}", carsCache.getSize());
-        carsCache.getKeys().forEach(key -> LOGGER.info("Key - [{}]", key));
-
-        Cache testCache = cacheManager.getCache("test_bean");
-        LOGGER.info("Test bean cache {}", testCache.getSize());
-        testCache.getKeys().forEach(key -> LOGGER.info("Key - [{}]", key));
-
-        LOGGER.info("Getting cars by firstRegistrationDate.");
-        LinkedList<CarEntity> carsByDate = new LinkedList<>(
-                repository.findAllByFirstRegistrationDateAfter(LocalDateTime.now().minusWeeks(50))
-        );
-        carsByDate.forEach(System.out::println);
-
-        LOGGER.info("Deleting cars from previous query.");
-        boolean deleteAll = repository.deleteAll(carsByDate);
-        LOGGER.info("Cars from previous query deleted [{}}].", deleteAll);
-
-        LOGGER.info("Deleting by Ids [{}] cars.", carByIds.size());
-        boolean deleteByIds = repository.deleteByIds(carByIds.stream().map(CarEntity::getId).collect(toSet()));
-        LOGGER.info("Cars from previous query deleted [{}].", deleteByIds);
-
-        carByIds = repository.findAll();
-        LOGGER.info("There are: {} cars.", carByIds.size());
-
-        final boolean queryResult = repository.executeUpdateQuery(
-                "update cars c set active = NOT active",
-                Collections.emptyMap()
-        );
-        LOGGER.info("Result is here {}", queryResult);
-
-        final List<CarEntity> carEntities = repository.executeSelectQuery(
-                "SELECT * FROM cars WHERE active = :active",
-                Stream.of("active").collect(toMap(value -> value, value -> true)),
-                fields -> new CarEntity(
-                        Long.valueOf(String.valueOf(fields[0])),
-                        Boolean.parseBoolean(String.valueOf(fields[1])),
-                        new LocalDateTimeConverter().convertToEntityAttribute(String.valueOf(fields[2])),
-                        new LocalDateTimeConverter().convertToEntityAttribute(String.valueOf(fields[3]))
-                )
-        );
-        LOGGER.info("{}", carEntities);*/
     }
 
     private static List<Long> batchCarSave(CarDao repository) {
@@ -153,6 +92,16 @@ public class Main {
         List<CarEntity> savedCars = repository.saveAll(cars);
         savedCars.forEach(System.out::println);
         return savedCars.stream().map(CarEntity::getId).collect(Collectors.toList());
+    }
+
+    private void play() {
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+        MutableConfiguration<String, String> config = new MutableConfiguration<>();
+        Cache<String, String> cache = cacheManager.createCache("simpleCache", config);
+        cache.put("key1", "value1");
+        cache.put("key2", "value2");
+        cacheManager.close();
     }
 
 }
